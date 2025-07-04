88d55badfda17ee59a1eaceb9c04782aad243b00
jonniebigodes <joaocontadesenvolvimento@gmail.com>
2025-07-04 21:56:24 +0100

    Extends AvatarList to contain some additional violations

--- src/components/AvatarList/AvatarList.stories.tsx
+++ src/components/AvatarList/AvatarList.stories.tsx
@@ -1,14 +1,22 @@
-/* 
-TODO: ADD modes
-*/
 import type { Meta, StoryObj } from '@storybook/react'
+import { Avatar } from 'components/Avatar/Avatar'
+
+import { allModes } from '../../../.storybook/modes'
 
 import { AvatarList } from './AvatarList'
 
 const meta = {
   title: 'Components/AvatarList',
   component: AvatarList,
-  tags: ['autodocs'],
+  parameters: {
+    chromatic: {
+      modes: {
+        s: allModes.s,
+        m: allModes.m,
+        l: allModes.l,
+      },
+    },
+  },
 } satisfies Meta<typeof AvatarList>
 
 export default meta
@@ -71,3 +79,36 @@ export const BigUserCount: Story = {
 }
 
 export const Empty: Story = {}
+
+export const LargeNumberOfUsers: Story = {
+  args: {
+    users: Array.from({ length: 50 }, (_, i) => ({
+      id: i.toString(),
+      name: `User ${i}`,
+      avatarUrl: 'https://avatars3.githubusercontent.com/u/1831709',
+    })),
+  },
+  render: (args) => {
+    const firstRow = args.users?.slice(0, 25) || []
+    const secondRow = args.users?.slice(25, 50) || []
+
+    return (
+      <div style={{ display: 'flex', flexDirection: 'column', gap: '8px' }}>
+        <div style={{ display: 'flex', alignItems: 'center', gap: '4px' }}>
+          {firstRow.map((user) => (
+            <div key={user.id}>
+              <Avatar key={user.id} username={user.name} src={user.avatarUrl || undefined} />
+            </div>
+          ))}
+        </div>
+        <div style={{ display: 'flex', alignItems: 'center', gap: '4px' }}>
+          {secondRow.map((user) => (
+            <div key={user.id}>
+              <Avatar key={user.id} username={user.name} src={user.avatarUrl || undefined} />
+            </div>
+          ))}
+        </div>
+      </div>
+    )
+  },
+}
